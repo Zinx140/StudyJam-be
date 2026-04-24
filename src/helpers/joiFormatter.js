a0c9e48c91b96fc0e451960aee8f045c5fc05651
Zinx140 <frostocean4@gmail.com>
2026-04-24 22:53:47 +0700

    init

--- src/helpers/joiFormatter.js
+++ src/helpers/joiFormatter.js
@@ -1,5 +1,5 @@
 const joiFormatter = (errors) => {
-  const err_messages = error.details.reduce((result, err) => {
+  const err_messages = errors.details.reduce((result, err) => {
     const key = err.context.key;
     if (key in result) {
       result[key].push(err.message);
